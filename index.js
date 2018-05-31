5f56d6d2021488aaf3659e392aa26eac96900c22
Saturn597 <saturn5971@gmail.com>
2018-05-31 03:55:22 +0000

    Pull token from a secrets file, more convenient

--- index.js
+++ index.js
@@ -1,19 +1,14 @@
 const Discord = require('discord.js');
 const readline = require('readline');
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-  terminal: false
-});
+const secrets = require('./secrets.js');
+const token = secrets.token;
 
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log('Logged in!')
+  console.log('Logged in!');
 });
 
-rl.once('line', (token) => {
-  client.login(token).
-    catch((e) => console.log('Login unsuccessful'));
-});
+client.login(token).
+  catch((e) => console.log('Login unsuccessful'));
