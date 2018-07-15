db11ce45b62c532f1f50f42db27a505d934a5562
Saturn597 <saturn5971@gmail.com>
2018-07-15 22:20:04 +0000

    If new messages come in, add to conversation

--- ConversationManager.js
+++ ConversationManager.js
@@ -1,11 +1,17 @@
 class ConversationManager {
-  constructor(client, maxMessages = 50) {
+  constructor(client, maxMessages = 50, onMessage = () => {}) {
     this.client = client;
     this.conversations = new Map();
     this.maxMessages = maxMessages;
+    this.onMessage = onMessage;
+
+    this.client.on('message', this.processMessage.bind(this));
   }
 
   getConversation(friend) {
+    // TODO: this should maybe search based on channels, not on friends (would
+    // make this more general - group DM channels and guild channels aren't
+    // necessarily tied to a single user).
     if (!friend.dmChannel) {
       return Promise.resolve(null);
     }
@@ -23,6 +29,19 @@ class ConversationManager {
         this.conversations.set(friend, messages);
         return messages;
       });
+  }
+
+  processMessage(message) {
+    if (message.channel.type === 'dm') {
+      this.getConversation(message.author).then(conv => {
+        if (!conv) {
+          // TODO: handle - this happens if, e.g., we wrote the message.
+          return;
+        }
+        conv.set(message.id, message);
+        this.onMessage(message);
+      });
+    }
   }
 }
 
